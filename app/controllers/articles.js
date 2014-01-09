c3f8600d374a766d4578be6246326ad425019a8c
jpmcvey <jpmcvey@gmail.com>
2014-01-09 15:03:31 -0600

    Update articles.js
    Change "a article" to "an article, lines 22 and 41

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -19,7 +19,7 @@ exports.article = function(req, res, next, id) {
 };
 
 /**
- * Create a article
+ * Create an article
  */
 exports.create = function(req, res) {
     var article = new Article(req.body);
@@ -38,7 +38,7 @@ exports.create = function(req, res) {
 };
 
 /**
- * Update a article
+ * Update an article
  */
 exports.update = function(req, res) {
     var article = req.article;
